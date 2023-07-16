40732bebaed3a2dce462012980ada3cee626b17e
upetrovi <una.petrovic@non.agilent.com>
2023-07-16 12:10:16 +0200

    Add project files.

---
+++ az-webapp-una/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_webapp_una.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
